c358a88509c50cf29fcc7be55e3a7c244c0518c5
dell <dell@dell-Inspiron-3558>
2019-08-26 15:50:07 +0530

    dao

---
+++ Spring_Basics/src/com/dev/dao/Coach.java
@@ -0,0 +1,8 @@
+package com.dev.dao;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+	
+	public String getDailyFortune();
+}
---
+++ Spring_Basics/src/com/dev/dao/FortuneService.java
@@ -0,0 +1,7 @@
+package com.dev.dao;
+
+public interface FortuneService {
+
+	
+	public String getFortune();
+}
